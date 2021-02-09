c957e6b311af0737f7d207e00370cbf338c4bff1
Jaydon Fernandes <60637128+JaydonFernandes@users.noreply.github.com>
2021-02-09 08:00:28 -0700

    added test Project

--- Tests/GroceryCoKioskTest/UnitTest1.cs
+++ Tests/GroceryCoKioskTest/UnitTest1.cs
@@ -3,10 +3,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace GroceryCoKioskTest
 {
     [TestClass]
-    public class UnitTest1
+    public class ProductTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void DefaultValuesTest()
         {
         }
     }
